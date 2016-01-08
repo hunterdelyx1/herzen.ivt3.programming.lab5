244cc195ee618b763a757fddfa478fcc28071209
orlovv <orlovv@herzen.spb.ru>
2016-01-08 23:15:23 +0300

    init

---
+++ src/main/java/ru/spb/herzen/ivt3/App.java
@@ -0,0 +1,27 @@
+package ru.spb.herzen.ivt3;
+
+public class App 
+{
+    // Sum
+    public int sum(int a, int b){
+        return a+b;
+    }
+
+    public String sum(String a, String b){
+        return a+b;
+    }
+
+    //Multiply
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+    public float multiply(float a, float b){
+        return a*b;
+    }
+
+    //Function
+    public float function(float x){
+        return 10/(12-x);
+    }
+}
